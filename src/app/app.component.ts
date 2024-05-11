a9bf178095c9909f39b1918f21db3eeaff9edf66
Knusbi <72132468+Knusbi@users.noreply.github.com>
2024-05-11 13:45:19 +0200

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {HttpService} from "./http.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Reisenthaler-Bestellsystem-Frontend';
+
+  tischname: String = "";
+
+  constructor(private http: HttpClient, private httpService: HttpService, private route: ActivatedRoute ) {
+  }
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.tischname = params['tischname'];
+      console.log(this.tischname);
+
+
+      this.httpService.getAllButtons().subscribe(
+        (data: any) => {
+          console.log("data");
+          console.log(data);
+
+        },
+        (error) => {
+          console.error('Error:', error);
+        }
+      );
+
+
+
+    });
 }
+}
+
