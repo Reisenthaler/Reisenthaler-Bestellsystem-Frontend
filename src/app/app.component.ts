7e4845d07f823bcbf843d8dff22e3c35c9d433bc
Knusbi <72132468+Knusbi@users.noreply.github.com>
2024-06-08 09:57:28 +0200

    design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,25 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
-import {HttpService} from "./http.service";
-import {MatTabChangeEvent} from "@angular/material/tabs";
-import {MatDialog} from "@angular/material/dialog";
-import {TextPopupComponent} from "./text-popup/text-popup.component";
-import {ZutatenPopupComponent} from "./zutaten-popup/zutaten-popup.component";
-
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { HttpService } from "./http.service";
+import { MatTabChangeEvent, MatTabGroup } from "@angular/material/tabs";
+import { MatDialog } from "@angular/material/dialog";
+import { TextPopupComponent } from "./text-popup/text-popup.component";
+import { ZutatenPopupComponent } from "./zutaten-popup/zutaten-popup.component";
 
 interface Zutat {
   id: number;
   name: string;
 }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'Reisenthaler-Bestellsystem-Frontend';
+  @ViewChild('tabGroup', { static: false }) tabGroup!: MatTabGroup;
 
   tischname: String = "";
   allButtons: any[] = [];
@@ -27,194 +28,158 @@ export class AppComponent implements OnInit{
   einkaufswagentab: boolean = true;
   totalPrice: any;
 
+  showBackButton: boolean = false;
+  showWeiterButton: boolean = false;
+
   navigationItems = [
-    {label: 'Bestellung', icon: 'home', route: '/Bestellung', id: 99999},
+    { label: 'Bestellung', icon: 'home', route: '/Bestellung', id: 99999 },
   ];
 
+  constructor(private http: HttpClient, private httpService: HttpService, private route: ActivatedRoute, public dialog: MatDialog) { }
 
-
-
-
-  constructor(private http: HttpClient, private httpService: HttpService, private route: ActivatedRoute, public dialog: MatDialog) {
-  }
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.tischname = params['tischname'];
       console.log(this.tischname);
-
     });
-
-      this.httpService.getAllButtons().subscribe(
-        (data: any) => {
-            this.allButtons = data;
-
-            console.log("allbuttons");
-          //  console.log(this.allButtons);
-
-            this.addButtonsToNavBar(this.allButtons);
-
-            this.httpService.startGettingUpdates();
-
-            this.httpService.triggerUpdate$.subscribe(() => {
-              this.handleUpdate();
-            });
-        },
-        (error) => {
-          console.error('Error:', error);
-        }
-      );
-  }
-
-  private handleUpdate() {
 
     this.httpService.getAllButtons().subscribe(
       (data: any) => {
         this.allButtons = data;
-//        console.log(this.allButtons);
+        console.log("allbuttons");
+        this.addButtonsToNavBar(this.allButtons);
+        this.httpService.startGettingUpdates();
+        this.httpService.triggerUpdate$.subscribe(() => {
+          this.handleUpdate();
+        });
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
 
-        this.currentButtons.forEach((button: any) =>{
-          if (!this.allButtons.includes(button))
-          {
+  ngAfterViewInit(): void {
+    // Check tabGroup initialization
+    if (this.tabGroup) {
+      // Access tabGroup properties or perform actions
+      this.tabGroup.selectedIndex = 0; // Example usage
+    } else {
+      console.error('Tab Group is not initialized.');
+    }
+  }
+
+  private handleUpdate() {
+    this.httpService.getAllButtons().subscribe(
+      (data: any) => {
+        this.allButtons = data;
+        this.currentButtons.forEach((button: any) => {
+          if (!this.allButtons.includes(button)) {
             this.currentButtons = this.currentButtons.filter(item => item !== button);
           }
         })
-
         this.httpService.sentUpToDate();
       });
-
-
   }
 
   onTabChange(event: MatTabChangeEvent) {
-    this.einkaufswagentab = event.tab.textLabel === "99999";
-
-    this.getCurrentButtons(event.tab.textLabel);
-
+    if(this.einkaufswagentab = event.tab.textLabel === "99999")
+    {
+      this.showWeiterButton = false;
+      this.showBackButton = false;
+    }
+    else
+      if (this.einkaufswagen.length != 0)
+      {
+        this.showWeiterButton = true;
+      }
+      this.getCurrentButtons(event.tab.textLabel);
     this.calculateTotalPrice();
   }
 
-
-
-  addButtonsToNavBar(allbuttons: any[])
-  {
+  addButtonsToNavBar(allbuttons: any[]) {
     allbuttons.forEach((button: any) => {
-      if (button.kategorie === '')
-      {
-        this.navigationItems.push({label: button.name, icon: 'home', route: '/' + button.name, id: button.id});
+      if (button.kategorie === '') {
+        this.navigationItems.push({ label: button.name, icon: 'home', route: '/' + button.name, id: button.id });
       }
     });
   }
 
-  getCurrentButtons(vorgaenger: any)
-  {
+  getCurrentButtons(vorgaenger: any) {
     this.currentButtons.length = 0;
-
     this.allButtons.forEach((button: any) => {
-
-      if (button.vorgaenger.endsWith("/"))
-      {
+      if (button.vorgaenger.endsWith("/")) {
         button.vorgaenger = button.vorgaenger.slice(0, -1);
       }
-
-      if (button.vorgaenger.includes('/'))
-      {
-        let dividedStrings;
-        dividedStrings = button.vorgaenger.split('/');
-
-        dividedStrings.forEach((vgaenger: any) =>{
-          if(vgaenger === vorgaenger)
-          {
-
+      if (button.vorgaenger.includes('/')) {
+        let dividedStrings = button.vorgaenger.split('/');
+        dividedStrings.forEach((vgaenger: any) => {
+          if (vgaenger === vorgaenger) {
             this.currentButtons.push(button);
           }
         })
       }
-      if (button.vorgaenger === vorgaenger)
-      {
+      if (button.vorgaenger === vorgaenger) {
         this.currentButtons.push(button);
       }
     });
   }
 
-  produktButtonClick(button: any)
-  {
+  produktButtonClick(button: any) {
+    if (this.hasUntermenue(button)) {
+     this.showBackButton = true;
 
-      if (this.hasUntermenue(button))
-      {
-//        console.log("a");
-        this.getCurrentButtons(button.id);
-      }
-      else
-      {
-        this.addToEinkaufswagen(button);
-      }
+      this.getCurrentButtons(button.id);
+    } else {
+      this.showWeiterButton = true;
+
+
+      this.addToEinkaufswagen(button);
+    }
   }
 
-  hasUntermenue(button: any)
-  {
+  hasUntermenue(button: any) {
     return this.allButtons.some(obj => obj.vorgaenger === button.id);
   }
 
-  addToEinkaufswagen(button: any)
-  {
-    if (this.einkaufswagen.includes(button))
-    {
+  addToEinkaufswagen(button: any) {
+    if (this.einkaufswagen.includes(button)) {
       this.einkaufswagen[this.einkaufswagen.indexOf(button)].quantity++;
-    }
-    else
-    {
+    } else {
       this.einkaufswagen.push(button);
       this.einkaufswagen[this.einkaufswagen.length - 1].ausgewaelteZutaten = "";
     }
-   console.log(this.einkaufswagen);
+    console.log(this.einkaufswagen);
   }
 
   submitOrder() {
-
     this.httpService.sendData(this.einkaufswagen, this.tischname).subscribe(response => {
-      console.log('Response:', response); // Log the response
+      console.log('Response:', response);
     }, error => {
-      console.error('Error:', error); // Log the error
+      console.error('Error:', error);
     });
-
     this.einkaufswagen.length = 0;
     this.calculateTotalPrice();
   }
 
   addItem(i: number) {
-    console.log(i);
     this.einkaufswagen[i].quantity++;
-
     this.calculateTotalPrice();
   }
 
   removeItem(i: number) {
     this.einkaufswagen[i].quantity--;
-
-    if (this.einkaufswagen[i].quantity <= 0 )
-    {
-      this.einkaufswagen.splice(i,1);
+    if (this.einkaufswagen[i].quantity <= 0) {
+      this.einkaufswagen.splice(i, 1);
     }
-
     this.calculateTotalPrice();
   }
 
-  goBack() {
-    if (this.allButtons.find(obj => obj.id === this.currentButtons[0].vorgaenger).vorgaenger != 0)
-    {
-      this.getCurrentButtons(this.allButtons.find(obj => obj.id === this.currentButtons[0].vorgaenger).vorgaenger);
-    }
-  }
-
-  calculateTotalPrice()
-  {
+  calculateTotalPrice() {
     this.totalPrice = 0;
-
-    // Iterate over each item in cartItems and sum up the prices
     for (const item of this.einkaufswagen) {
       this.totalPrice += item.preis * item.quantity;
     }
-
     this.totalPrice = this.totalPrice.toFixed(2);
   }
 
@@ -226,7 +191,6 @@ export class AppComponent implements OnInit{
     { id: 3, name: 'Zutat 3' }
   ];
   selectedZutaten: number[] = [];
- // showZutatenPopup: boolean = false; // Flag to control popup visibility
 
   openZutatenPopup(i: number) {
     this.zutatenPopup.produkt = i;
@@ -238,81 +202,78 @@ export class AppComponent implements OnInit{
 
     dialogRef.afterClosed().subscribe((result: { selectedZutaten: number[], produktId: number }) => {
       if (result != null) {
-       // this.selectedZutaten = result.selectedZutaten;
-this.setZutaten(result.produktId, result.selectedZutaten);
-
-
+        this.setZutaten(result.produktId, result.selectedZutaten);
       }
     });
-}
-getZutaten(i: number)
-{
-  this.zutaten.length = 0;
+  }
 
-  this.allButtons.forEach(button => {
-    if (this.einkaufswagen[i].moeglicheZutaten.endsWith("/"))
-    {
-      this.einkaufswagen[i].moeglicheZutaten = this.einkaufswagen[i].moeglicheZutaten.slice(0, -1);
-    }
-    if (this.einkaufswagen[i].moeglicheZutaten.includes('/'))
-    {
-      let dividedStrings;
-      dividedStrings = this.einkaufswagen[i].moeglicheZutaten.split('/');
+  getZutaten(i: number) {
+    this.zutaten.length = 0;
+    this.allButtons.forEach(button => {
+      if (this.einkaufswagen[i].moeglicheZutaten.endsWith("/")) {
+        this.einkaufswagen[i].moeglicheZutaten = this.einkaufswagen[i].moeglicheZutaten.slice(0, -1);
+      }
+      if (this.einkaufswagen[i].moeglicheZutaten.includes('/')) {
+        let dividedStrings = this.einkaufswagen[i].moeglicheZutaten.split('/');
+        dividedStrings.forEach((vgaenger: any) => {
+          if (vgaenger === button.vorgaenger) {
+            this.zutaten.push({ id: button.id, name: button.name });
+          }
+        })
+      }
+    });
 
-      dividedStrings.forEach((vgaenger: any) =>{
-        if(vgaenger === button.vorgaenger)
-        {
-          this.zutaten.push({id: button.id, name: button.name});
-        }
-      })
-    }
+    this.selectedZutaten.length = 0;
+    this.allButtons.forEach(button => {
+      if (this.einkaufswagen[i].ausgewaelteZutaten.includes(';')) {
+        let dividedStrings = this.einkaufswagen[i].ausgewaelteZutaten.split(';');
+        dividedStrings.forEach((zutaten: any) => {
+          if (zutaten === button.id) {
+            this.selectedZutaten.push(button.id);
+          }
+        })
+      } else if (this.einkaufswagen[i].ausgewaelteZutaten === button.id) {
+        this.selectedZutaten.push(button.id);
+      }
+    });
+  }
 
-
-  });
-
-  this.selectedZutaten.length = 0;
-
-  this.allButtons.forEach(button => {
-
-    if (this.einkaufswagen[i].ausgewaelteZutaten.includes(';'))
-    {
-      let dividedStrings;
-      dividedStrings = this.einkaufswagen[i].ausgewaelteZutaten.split(';');
-
-      dividedStrings.forEach((zutaten: any) =>{
-        if(zutaten === button.id)
-        {
-          this.selectedZutaten.push(button.id);
-        }
-      })
-    }
-    else if(this.einkaufswagen[i].ausgewaelteZutaten === button.id)
-    {
-      this.selectedZutaten.push(button.id);
-    }
-
-
-
-      });
-
-}
-
-setZutaten(i:number, ausgewaelteZutaten: any[])
-{
-      this.einkaufswagen[i].ausgewaelteZutaten = ausgewaelteZutaten.join(";");
-}
-
+  setZutaten(i: number, ausgewaelteZutaten: any[]) {
+    this.einkaufswagen[i].ausgewaelteZutaten = ausgewaelteZutaten.join(";");
+  }
 
   @ViewChild('popup') popup!: TextPopupComponent;
 
-  addInfo(i: number)
-  {
+  addInfo(i: number) {
     this.popup.open(this.einkaufswagen[i].info, i);
   }
 
-  handleSubmit(eventData: { userInput: string, selectedProdukt: number })
-  {
-      this.einkaufswagen[eventData.selectedProdukt].info = eventData.userInput;
+  handleSubmit(eventData: { userInput: string, selectedProdukt: number }) {
+    this.einkaufswagen[eventData.selectedProdukt].info = eventData.userInput;
+  }
+
+  goBack() {
+    this.getCurrentButtons(this.allButtons.find(obj => obj.id === this.currentButtons[0].vorgaenger).vorgaenger);
+    this.showBackButton = false;
+  }
+
+  goWeiter() {
+    setTimeout(() => {
+      if (this.tabGroup) {
+        this.tabGroup.selectedIndex = 0;
+        const event: MatTabChangeEvent = {
+          index: 0,
+          tab: {
+            textLabel: "99999",
+            disabled: false
+          } as any
+        };
+        this.onTabChange(event);
+        this.getCurrentButtons("99999");
+        this.calculateTotalPrice();
+      } else {
+        console.error('Tab Group is not initialized.');
+      }
+    });
   }
 }
-
